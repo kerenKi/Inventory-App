ab58b05d52ce88ee0dd7618e7914c69d827ed6a2
kerenKi <kkinberg13@gmail.com>
2019-08-01 11:37:09 +0200

    Display details and inventory

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ const app = new Vue({
     product: 'Socks',
     image:'./assets/Socks-green-onWhite.jpg',
     description: 'warm and fuzzy. good for the winter',
-    videoLink: "https://youtu.be/CL4hENeteno"
+    details: ["80% cotton", "20% polyester", "Gener-neutral"],
+    videoLink: "https://youtu.be/CL4hENeteno",
+    inventory: 10,
+    onSale: true,
+    variants: [
+      {
+        variantId: 1,
+        variantColor: "green"
+      },
+      {
+        variantId: 2,
+        variantColor: "blue"
+      }
+    ]
   }
 })
