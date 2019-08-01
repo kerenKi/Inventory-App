a5910376d338698bef5dd14b06746eb197281700
kerenKi <kkinberg13@gmail.com>
2019-08-01 15:42:05 +0200

    Refactor all tamplate to 'product' component

--- main.js
+++ main.js
@@ -1,35 +1,81 @@
-const app = new Vue({
-  el:"#app",
-  data: {
-    brand: 'Vue Mastery',
-    product: 'Socks',
-    selectedVariant: 0,
-    description: 'warm and fuzzy. good for the winter',
-    details: ["80% cotton", "20% polyester", "Gener-neutral"],
-    inventory: 12,
-    sizes: ["32-34","36-38","40-42","44-46"],
-    variants: [
-      {
-        variantId: 1,
-        variantColor: "green",
-        variantImage: "./assets/Socks-green-onWhite.jpg",
-        variantQuantity: 10,
-        onSale: true,
+Vue.component('product',{
+  template: `
+  <div class="product">
 
+    <div class="product-image">
+      <img v-bind:src="image" alt="Socks">
+    </div>
 
-      },
-      {
-        variantId: 2,
-        variantColor: "blue",
-        variantImage: "./assets/Socks-blue-onWhite.jpg",
-        variantQuantity: 0,
-        onSale: false,
+    <div class="product-info">
+      <h1>{{ title }}</h1>
+      <p>{{description ? description : 'No description Available'}}</p> 
+      <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+      </ul>
+      <p>Size range:</p>
+      <ul>
+        <li v-for="size in sizes">{{size}}</li>
+      </ul>
+      
+      <p>{{ onSale }}</p>
 
+      <div v-for="(variant, index) in variants" 
+            :key="variant.variantId"
+            class="color-box"
+            :style="{ backgroundColor: variant.variantColor }"
+            @mouseover="updateProduct(index)">
+      </div>
+      
+      <p v-if="inStock"> In stock </p>
+      <p v-else-if="inventory <= 10 && inventory > 0">Almost gone!</p>
+      <p v-else="inventory = 0"
+        :class="{ outOfStock: !inStock }"> Out of stock </p>
+      
 
-      }
-    ],
-    cart: 0,
-    },
+      <button v-on:click="addToCart"
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }"
+              >Add to cart</button>
+      <button v-on:click="removeFromCart">Remove from cart</button>
+      <div class="cart">
+        <p>Cart: {{ cart }}</p>
+      </div>
+
+    </div>
+
+  </div>`,
+  data() {
+    return {
+      brand: 'Vue Mastery',
+      product: 'Socks',
+      selectedVariant: 0,
+      description: 'warm and fuzzy. good for the winter',
+      details: ["80% cotton", "20% polyester", "Gener-neutral"],
+      inventory: 12,
+      sizes: ["32-34","36-38","40-42","44-46"],
+      variants: [
+        {
+          variantId: 1,
+          variantColor: "green",
+          variantImage: "./assets/Socks-green-onWhite.jpg",
+          variantQuantity: 10,
+          onSale: true,
+  
+  
+        },
+        {
+          variantId: 2,
+          variantColor: "blue",
+          variantImage: "./assets/Socks-blue-onWhite.jpg",
+          variantQuantity: 0,
+          onSale: false,
+  
+  
+        }
+      ],
+      cart: 0,
+    }
+  },
   methods: {
     addToCart () {
       this.cart += 1
@@ -63,4 +109,8 @@ const app = new Vue({
     }
 
   }
+})
+
+const app = new Vue({
+  el:"#app",
 })
